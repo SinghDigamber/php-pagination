70a2eddbbe8ada485e5c1baef482116b906dcbb1
PositronX.io <digamber@positronx.io>
2023-09-21 20:56:10 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,126 @@
-<!-- jQuery + Bootstrap JS -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<?php 
+  // Database
+  include('config/db.php');
+  
+  // Set session
+  session_start();
+  if(isset($_POST['records-limit'])){
+      $_SESSION['records-limit'] = $_POST['records-limit'];
+  }
+  
+  $limit = isset($_SESSION['records-limit']) ? $_SESSION['records-limit'] : 5;
+  $page = (isset($_GET['page']) && is_numeric($_GET['page']) ) ? $_GET['page'] : 1;
+  $paginationStart = ($page - 1) * $limit;
+  $authors = $connection->query("SELECT * FROM authors LIMIT $paginationStart, $limit")->fetchAll();
 
-<script>
-    $(document).ready(function () {
-        $('#records-limit').change(function () {
-            $('form').submit();
-        })
-    });
-</script>
+  // Get total records
+  $sql = $connection->query("SELECT count(id) AS id FROM authors")->fetchAll();
+  $allRecrods = $sql[0]['id'];
+  
+  // Calculate total pages
+  $totoalPages = ceil($allRecrods / $limit);
+
+  // Prev + Next
+  $prev = $page - 1;
+  $next = $page + 1;
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <title>PHP Pagination Example</title>
+    <style>
+        .container {
+            max-width: 1000px
+        }
+
+        .custom-select {
+            max-width: 150px
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h2 class="text-center mb-5">Simple PHP Pagination Demo</h2>
+
+
+        <!-- Select dropdown -->
+        <div class="d-flex flex-row-reverse bd-highlight mb-3">
+            <form action="index.php" method="post">
+                <select name="records-limit" id="records-limit" class="custom-select">
+                    <option disabled selected>Records Limit</option>
+                    <?php foreach([5,7,10,12] as $limit) : ?>
+                    <option
+                        <?php if(isset($_SESSION['records-limit']) && $_SESSION['records-limit'] == $limit) echo 'selected'; ?>
+                        value="<?= $limit; ?>">
+                        <?= $limit; ?>
+                    </option>
+                    <?php endforeach; ?>
+                </select>
+            </form>
+        </div>
+
+        <!-- Datatable -->
+        <table class="table table-bordered mb-5">
+            <thead>
+                <tr class="table-success">
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">DOB</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($authors as $author): ?>
+                <tr>
+                    <th scope="row"><?php echo $author['id']; ?></th>
+                    <td><?php echo $author['first_name']; ?></td>
+                    <td><?php echo $author['last_name']; ?></td>
+                    <td><?php echo $author['email']; ?></td>
+                    <td><?php echo $author['birthdate']; ?></td>
+                </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+
+        <!-- Pagination -->
+        <nav aria-label="Page navigation example mt-5">
+            <ul class="pagination justify-content-center">
+                <li class="page-item <?php if($page <= 1){ echo 'disabled'; } ?>">
+                    <a class="page-link"
+                        href="<?php if($page <= 1){ echo '#'; } else { echo "?page=" . $prev; } ?>">Previous</a>
+                </li>
+
+                <?php for($i = 1; $i <= $totoalPages; $i++ ): ?>
+                <li class="page-item <?php if($page == $i) {echo 'active'; } ?>">
+                    <a class="page-link" href="index.php?page=<?= $i; ?>"> <?= $i; ?> </a>
+                </li>
+                <?php endfor; ?>
+
+                <li class="page-item <?php if($page >= $totoalPages) { echo 'disabled'; } ?>">
+                    <a class="page-link"
+                        href="<?php if($page >= $totoalPages){ echo '#'; } else {echo "?page=". $next; } ?>">Next</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+
+    <!-- jQuery + Bootstrap JS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script>
+        $(document).ready(function () {
+            $('#records-limit').change(function () {
+                $('form').submit();
+            })
+        });
+    </script>
+</body>
+
+</html>
